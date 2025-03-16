7f2082fd268af41dc8ee0a3f6d17634199271b33
Ben Benyamin <benbin52+git@gmail.com>
2025-03-16 14:48:20 -0500

    Showing Totodile now!

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@ int main()
     std::vector<float> cameraNormal = {0.0f, 0.0f, 1.0f};
     canvas.setCameraNormal(cameraNormal);
 
-    std::string filename = "/home/ben/Documents/Courses/Winter-2025/CPP/Project/example/eiffel.stl";
+    std::string filename = "/home/ben/Documents/Courses/Winter-2025/CPP/Project/example/ASCII.stl";
     std::vector<TriangleSurface> triangles = readSTL(filename);
     std::cout << "Loaded " << triangles.size() << " triangles from " << filename << std::endl;
 
@@ -23,23 +23,23 @@ int main()
     canvas.clear();
     for (auto &triangle : triangles) 
     {
-        triangle.scale(7);
-        triangle.translate(500,500,0);
-        triangle.rotateAroundX(90,rotationCenter);
-        triangle.rotateAroundZ(-90,rotationCenter);
-        // triangle.rotateAroundX(-15,rotationCenter);
+        triangle.scale(4.5);
+        triangle.rotateAroundY(-90,rotationCenter);
+        triangle.translate(-200,0,0);
+        // triangle.rotateAroundZ(-90,rotationCenter);
+        triangle.rotateAroundY(-15,rotationCenter);
 
     }
 
-    for (int i =0 ;i < 360/15; i ++)
+    for (int i =0 ;i < 360/6; i ++)
     {
 
         for (auto &triangle : triangles)
         {
             triangle.project(canvas);
-            triangle.rotateAroundX(15,rotationCenter);
+            triangle.rotateAroundX(6,rotationCenter);
         }
-        std::string outFileName = "../output/cube_" + std::to_string(i) + ".ppm";
+        std::string outFileName = "../output/MODEL_" + std::to_string(i) + ".ppm";
         canvas.writePPM(outFileName);
         canvas.clear();
     
