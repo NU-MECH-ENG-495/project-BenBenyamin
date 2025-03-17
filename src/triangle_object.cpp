851c56cc31163273ced51031a129fdd8efd29f2d
Ben Benyamin <benbin52+git@gmail.com>
2025-03-16 19:41:31 -0500

    Added open MP to methods in triangle_object.cpp

--- src/triangle_object.cpp
+++ src/triangle_object.cpp
@@ -18,6 +18,7 @@ void TriangleObject::project(Canvas &c)
 
 void TriangleObject::rotateAroundX(float angle, const std::vector<float> &rotationPoint)
 {
+    #pragma omp parallel for
     for (int i = 0; i < triangles->size(); ++i)
     {
         (*triangles)[i].rotateAroundX(angle, rotationPoint);
@@ -26,6 +27,7 @@ void TriangleObject::rotateAroundX(float angle, const std::vector<float> &rotati
 
 void TriangleObject::rotateAroundY(float angle, const std::vector<float> &rotationPoint)
 {
+    #pragma omp parallel for
     for (int i = 0; i < triangles->size(); ++i)
     {
         (*triangles)[i].rotateAroundY(angle, rotationPoint);
@@ -34,6 +36,7 @@ void TriangleObject::rotateAroundY(float angle, const std::vector<float> &rotati
 
 void TriangleObject::rotateAroundZ(float angle, const std::vector<float> &rotationPoint)
 {
+    #pragma omp parallel for
     for (int i = 0; i < triangles->size(); ++i)
     {
         (*triangles)[i].rotateAroundZ(angle, rotationPoint);
@@ -42,6 +45,7 @@ void TriangleObject::rotateAroundZ(float angle, const std::vector<float> &rotati
 
 void TriangleObject::scale(float k)
 {
+    #pragma omp parallel for
     for (int i = 0; i < triangles->size(); ++i)
     {
         (*triangles)[i].scale(k);
@@ -50,6 +54,7 @@ void TriangleObject::scale(float k)
 
 void TriangleObject::translate(float x, float y, float z)
 {
+    #pragma omp parallel for
     for (int i = 0; i < triangles->size(); ++i)
     {
         (*triangles)[i].translate(x, y, z);
